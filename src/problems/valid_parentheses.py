92fa71bd5e6894745fd5a26c0fde80d65b714c7d
sashak2 <ak.sahashi@gmail.com>
2026-02-02 17:47:45 -0800

    20. Valid Parentheses

--- src/problems/valid_parentheses.py
+++ src/problems/valid_parentheses.py
@@ -2,22 +2,35 @@
 
 class ValidParentheses:
     parentheses = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
+        ")": "(",
+        "]": "[",
+        "}": "{",
     }
 
     def test_case(self):
         print(__name__)
         self.isValid("()")
-        self.isValid("()[]/{/}")
+        self.isValid("()[]{}")
         self.isValid("(]")
         self.isValid("([])")
         self.isValid("([)]")
+        self.isValid("[")
+        self.isValid("((")
+        self.isValid("]")
         return
 
     def isValid(self, s: str) -> bool:
         print('----- start -----', s)
         result = True
-        print('----- end result -----', result)
+        stack = []
+        for char in s:
+            if char in self.parentheses:
+                if len(stack) == 0 or self.parentheses[char] != stack.pop():
+                    result = False
+                    break
+            else:
+                stack.append(char)
+        if len(stack) > 0:
+            result = False
+        print('result', result)
         return result
